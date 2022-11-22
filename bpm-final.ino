9fed855da0650a54ca23f94cc6e3d11eeb927f5e
INLL <waileungsunny@gmail.com>
2022-11-22 14:01:56 +0800

    Added comments and removed unnecessary codes

--- bpm-final.ino
+++ bpm-final.ino
@@ -1,10 +1,10 @@
 #include "TimerOne.h"
-#include <LiquidCrystal.h> //Call the LiquidCrystal library
+#include <LiquidCrystal.h>
 
-// acquire signal connected to the selected analog pin
+// Acquire signal connected to the selected analog pin
 int analogPin = A0;
 
-// global variable to store the value from the analog pin
+// Global variable to store the value from the analog pin
 int ECG = 0;
 int adc_flag = 0;
 
@@ -21,10 +21,11 @@ int index = 0;
 // Last time the bpm was shown
 long lastTime;
 
-// Display BPM in LCD
+// Initialize Liquid Crysal Display (LCD)
 const int rs = 12, en = 11, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+// Heart drawing 
 byte heart1[8] = {B11111, B11111, B11111, B11111, B01111, B00111, B00011, B00001};
 byte heart2[8] = {B00011, B00001, B00000, B00000, B00000, B00000, B00000, B00000};
 byte heart3[8] = {B00011, B00111, B01111, B11111, B11111, B11111, B11111, B01111};
@@ -35,25 +36,16 @@ byte heart7[8] = {B11000, B10000, B00000, B00000, B00000, B00000, B00000, B00000
 byte heart8[8] = {B11111, B11111, B11111, B11111, B11110, B11100, B11000, B10000};
 
 // This 'setup' function will be called once when Arduino board powers on or is reset.
-void setup()
-{
+void setup() {
   pinMode(8, INPUT); // Setup for leads off detection LO +
   pinMode(9, INPUT); // Setup for leads off detection LO -
-
-  // set a timer of length in microseconds
-  // Timer1 will generate an interrupt with the designated time interval
-  // The interrupt or data sampling frequency is ( 1e6 / time interval number in microseconds )
-
-  Timer1.initialize(500);
-  Timer1.attachInterrupt( timerIsr );
-
-  // attach the service routine here
-  // the function 'timerIsr' will be called at every timer interruptevent
   
-  Serial.begin (9600); // setup serial port
+  // Setup serial port
+  Serial.begin (230400);
   // Baud rate of 230400 can securely support 4 kB/s data transfer rate between Arduino and PC serial port
+  // 230400 is also the baud rate used in Project 3 in lab 4608 
   
-  // set up the LCD's number of columns and rows:
+  // Set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
 
   lcd.createChar(1, heart1);
@@ -66,51 +58,41 @@ void setup()
   lcd.createChar(8, heart8);
 }
 
-void timerIsr()
-{
-ECG = analogRead(analogPin); // read the input pin
-//Serial.println(ECG); // send the acquired data to serial port for saving on PC
-adc_flag = 1;
-}
-
 // This 'loop' function will be executed repeatedly
-void loop()
-{
-  if (adc_flag)
-{
-  adc_flag = 0;
-  //Wait for a bit to keep serial data from saturating
-//  delay(1);
-  }
+void loop() {
   sampleBuffer[index] = analogRead(A0);
 
   // Show results every 500 ms once the buffer is filled
-  if(nSamples >= N_SAMPLES && millis()-lastTime > 500) {
-    lastTime = millis();
+  if (nSamples >= N_SAMPLES && millis()-lastTime > 500) {
+    lastTime = millis();  // End of last time window
     
+    // Define baselines
     int maxValue = 0;
     int minValue = 1024;
     
-    // Calculate maximum and minimum
-    for(int i = 0; i < N_SAMPLES; i++) {
+    // Dynamically set threshold based on max and min in sampleBuffer
+    // Ideally, max and min represent the R and S wave values
+    for (int i = 0; i < N_SAMPLES; i++) {
       maxValue = max(sampleBuffer[i], maxValue);
       minValue = min(sampleBuffer[i], minValue); 
     }
   
+    // Threshold is determined as 70% of diff between Max and Min, plus Min
     float diff = max(maxValue - minValue, minDiff);
     float threshold = diff * rThreshold + minValue;
+
     int nHeartbeats = 0;
     int tHeartbeats = 0;
     int lastHeartbeat = 0;
     
-    for(int i = 1; i < N_SAMPLES; i++) {
-      if(sampleBuffer[(index+i+1)%N_SAMPLES] >= threshold 
+    for (int i = 1; i < N_SAMPLES; i++) {
+      if (sampleBuffer[(index+i+1)%N_SAMPLES] >= threshold 
           && sampleBuffer[(index+i)%N_SAMPLES] < threshold) {
         // Save time difference and increase counter
         // Don't count time difference if it is too short or too long
         // - 15 measurements correspond to roughly 250 bpm
         // - 150 measurements correspond to roughly 25 bpm
-        if(lastHeartbeat && i-lastHeartbeat > 15 && i-lastHeartbeat < 150) {
+        if (lastHeartbeat && i-lastHeartbeat > 15 && i-lastHeartbeat < 150) {
           tHeartbeats += i-lastHeartbeat; 
           nHeartbeats++;
         }
@@ -125,11 +107,11 @@ void loop()
     
     // Show results if enough heartbeats are found
     // In 5 s there should be 4 at 50 bpm
-//    if(nHeartbeats > 3) {
-//      Serial.print("Heart Rate (bpm): ");
-//      Serial.println(bpm);
+    //    if(nHeartbeats > 3) {
+    //      Serial.print("Heart Rate (bpm): ");
+    //      Serial.println(bpm);
 
-    if(bpm >= 60 && bpm <= 120) {
+    if (bpm >= 60 && bpm <= 120) {
       Serial.println("Heart Rate (bpm): ");
       Serial.print(bpm);
 
@@ -159,7 +141,7 @@ void loop()
       lcd.print("BPM     ");
 
     }
-    else if(bpm > 120){
+    else if (bpm > 120) {
       Serial.println("Warning!");
       Serial.print("Heart Rate (bpm): ");
       Serial.print(bpm);
